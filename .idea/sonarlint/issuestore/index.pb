60f61f4235d4db23ea21dba2a06f7a65fcd24857
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-12 20:51:34 +0200

    feat: add base.html

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+M
+Exchange/Exchange/settings.py,9\c\9c39fbc24243f8ec7c06396a79b61d20b93658f7
+I
+Exchange/Exchange/urls.py,7\e\7ee3a7c5e35870d84fc659cadfb51a68e7e85e35
--- Exchange/dashboard/views.py
+++ Exchange/dashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'dashboard/home.html', {'title': 'Home - Dashboard'})
+    return render(request, 'dashboard/home.html', {'title': 'Dashboard', 'subtitle': 'Home'})
