5d40955e55f020fd35a202b39472f72e95148a19
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-12 22:33:03 +0200

    feat: add href links to login and register pages

--- Exchange/users/views.py
+++ Exchange/users/views.py
@@ -9,9 +9,9 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Dear {username}, you have been successfully signed up!")
             return redirect('login')
     else:
         form = UserRegisterForm()
 
-    return render(request, "users/register.html", {"form": form})
+    messages.error(request, "Dear bro, you failed!")
+    return render(request, "users/register.html", {"title": "Register", "form": form})
