7d3c17b69f0a488ced01c3f5a2490ea5fea3b646
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-12 21:23:43 +0200

    feat: add login.html page

--- Exchange/Exchange/settings.py
+++ Exchange/Exchange/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "dashboard.apps.DashboardConfig",
+    "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- Exchange/Exchange/urls.py
+++ Exchange/Exchange/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('dashboard.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', extra_context={'title': 'Login'}), name='login'),
 ]
