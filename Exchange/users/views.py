2849f6f64f91ef515fe2fc1fb7cc56ee39ae2818
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-14 22:23:58 +0200

    feat: add error fields messages to login form and register form

--- Exchange/users/views.py
+++ Exchange/users/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 
 
 def register(request):
+
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
@@ -11,9 +12,6 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f"Dear {username}, you have been successfully signed up!")
             return redirect('login')
-        else:
-            for error in form.errors.values():
-                messages.error(request, f"{error}", extra_tags='danger')
 
     else:
         form = UserRegisterForm()
