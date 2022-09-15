ed4b267f314c532671e61205b9db02b2c232fa3b
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-15 18:10:29 +0200

    feat: add logout page

--- Exchange/Exchange/urls.py
+++ Exchange/Exchange/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('dashboard.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', extra_context={'title': 'Login'}), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html', extra_context={'title': 'Logout'}), name='logout'),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
