38fd71ba590bb004b9353af8d470b3c95dcc3332
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-15 17:11:32 +0200

    feat: add profile image

--- Exchange/Exchange/urls.py
+++ Exchange/Exchange/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', extra_context={'title': 'Login'}), name='login'),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
