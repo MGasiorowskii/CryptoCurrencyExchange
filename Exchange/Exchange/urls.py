4bfc2e1774c7c2415c16ce714d1f633362ed4546
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-16 20:24:04 +0200

    feat: add change password to profile

--- Exchange/Exchange/urls.py
+++ Exchange/Exchange/urls.py
@@ -20,6 +20,7 @@ from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('dashboard.urls')),
