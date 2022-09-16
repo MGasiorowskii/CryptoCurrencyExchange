d9486b8cc0cce46e4fc74733667a79506171ddcf
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-16 23:28:05 +0200

    feat: add reset password form

--- Exchange/users/views.py
+++ Exchange/users/views.py
@@ -4,6 +4,19 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset_form.html'
+    email_template_name = 'users/password_reset_email.html'
+    subject_template_name = 'users/password_reset_subject.txt'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = '/login'
 
 
 def register(request):
