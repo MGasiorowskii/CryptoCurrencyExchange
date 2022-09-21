1b50612a6a3782ebb3b498b2c47dae09e3e825ed
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-21 19:38:20 +0200

    feat: add api view to update db

--- Exchange/API/views.py
+++ Exchange/API/views.py
@@ -1,10 +1,36 @@
-import django.template.backends.django
 from rest_framework.viewsets import generics
 from .serializers import TokenSerializer
 from .models import BtcHistory
+from rest_framework.views import APIView
+from rest_framework.mixins import ListModelMixin
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+import datetime
+import pytz
 
 
-class UpdateAllData(generics.ListAPIView):
+class GetAllData(generics.ListAPIView):
     serializer_class = TokenSerializer
-    queryset = BtcHistory.objects.all()
+    queryset = BtcHistory.objects.all().order_by('date_time')
 
+
+class UpdateAllData(APIView, ListModelMixin):
+
+    def get(self, request):
+        for days in [365, 90, 1]:
+            url = f'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days={days}'
+            res = requests.get(url=url)
+            data = res.json()
+
+            for timestamp, price in data['prices']:
+
+                coin_price = round(price, 2)
+                timestamp = int(timestamp / 1000)
+                date_time = datetime.datetime.fromtimestamp(timestamp, tz=pytz.UTC)
+
+                if not BtcHistory.objects.filter(date_time=date_time).exists():
+                    BtcHistory.objects.create(price=coin_price,
+                                              date_time=date_time)
+
+        return Response(status=status.HTTP_202_ACCEPTED)
