75aaa6835285cf14a86348cd41b34dc8d86e0378
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-23 20:36:07 +0200

    feat: add view with tokens do dasboard

--- Exchange/wallet/models.py
+++ Exchange/wallet/models.py
@@ -6,6 +6,7 @@ class Token(models.Model):
     name = models.TextField(blank=False)
     symbol = models.TextField(max_length=10, blank=False)
     actual_price = models.FloatField(blank=False)
+    image = models.ImageField(default='bitcoin_icon.jpg', upload_to='token_logo')
 
     def __str__(self):
         return f"{self.name}"
