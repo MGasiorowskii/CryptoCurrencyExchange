fccedc77e3b2751136266183e8cff64fed6135f7
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-09-25 22:51:38 +0200

    feat: add form validation

--- Exchange/dashboard/views.py
+++ Exchange/dashboard/views.py
@@ -1,12 +1,29 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.shortcuts import render
-from wallet.models import Token
+from wallet.models import Token, Wallet
+from .forms import BuyForm, SellForm
 
 
 @login_required
 def home(request):
     tokens = Token.objects.all()
 
+    actual_price = Token.objects.get(name="bitcoin").actual_price
+    user_pk = request.user.pk
+    token_quantity = Wallet.objects.get(owner=user_pk, token=1).quantity
+
+    if request.method == "POST":
+        buy_form = BuyForm(request.POST)
+        if buy_form.is_valid():
+            messages.success(request, f"its work ")
+
+    else:
+        buy_form = BuyForm()
+
     return render(request, 'dashboard/home.html', {'title': 'Dashboard',
                                                    'subtitle': 'Home',
-                                                   'tokens': tokens})
+                                                   'tokens': tokens,
+                                                   'price': actual_price,
+                                                   'form': buy_form,
+                                                   'quantity': token_quantity})
